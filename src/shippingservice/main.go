b72f1c355934bfcc1baa63bb951a48cffddd1cd5
fatihreha <fatihr2003@gmail.com>
2025-08-10 20:54:39 +0300

    Jenkins Scenario 2 Test

--- src/shippingservice/main.go
+++ src/shippingservice/main.go
@@ -53,7 +53,7 @@ func init() {
 }
 
 func main() {
-	log.Printf("GHActions TEST: shipping service called with context")
+	log.Printf("JENKINS TEST: shipping service called with context")
 
 	if os.Getenv("DISABLE_TRACING") == "" {
 		log.Info("Tracing enabled, but temporarily unavailable")
